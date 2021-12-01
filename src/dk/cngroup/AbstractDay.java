d577dec9a90b36bace59051731ae33c7e1604b7c
David Kostka <dkostka@eurofunk.com>
2021-12-01 08:25:06 +0100

    Add day1 2021 solution

---
+++ src/dk/cngroup/AbstractDay.java
@@ -0,0 +1,24 @@
+package dk.cngroup;
+
+import java.io.IOException;
+
+/**
+ * Created by dkostka on 12/1/2021.
+ */
+public abstract class AbstractDay {
+    public abstract String getDay();
+    public abstract Object getPuzzle(String day) throws IOException;
+    public abstract Object partOne(Object puzzle);
+    public abstract Object partTwo(Object puzzle);
+
+    void printResult(final Object partOne, final Object partTwo) {
+        System.out.println("Part One solution: " + partOne.toString());
+        System.out.println("Part Two solution: " + partTwo.toString());
+    }
+
+    public void solve() throws IOException {
+        var day = getDay();
+        var puzzle = getPuzzle(day);
+        this.printResult(partOne(puzzle), partTwo(puzzle));
+    }
+}
---
+++ src/dk/cngroup/year2021/Day1.java
@@ -0,0 +1,50 @@
+package dk.cngroup.year2021;
+
+import dk.cngroup.AbstractDay;
+import dk.cngroup.Utils;
+
+import java.io.IOException;
+
+
+public class Day1 extends AbstractDay {
+
+    public static void main(String args[]) throws IOException {
+        Day1 day = new Day1();
+        day.solve();
+    }
+
+    @Override
+    public String getDay() {
+        return "1";
+    }
+
+    @Override
+    public Object getPuzzle(final String day) throws IOException {
+        return Utils.getLinesToIntegerArray(getDay());
+    }
+
+    @Override
+    public Object partOne(final Object puzzle) {
+        Integer[] input = (Integer[]) puzzle;
+        int res = 0;
+        for (int i = 1; i<input.length; i++) {
+            if (input[i-1] < input[i]) {
+                res++;
+            }
+        }
+        return res;
+    }
+
+    @Override
+    public Object partTwo(final Object puzzle) {
+        Integer[] input = (Integer[]) puzzle;
+        int res = 0;
+        for(int i = 3; i<input.length; i++){
+            if(input[i-3] + input[i-2] + input[i-1] < input[i] + input[i-2] + input[i-1]){
+                res++;
+            }
+        }
+        return res;
+    }
+}
+
